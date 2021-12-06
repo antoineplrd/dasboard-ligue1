155ae6cf9c3cd951391c18acfaecadd63e40eec3
Pouillard <antoinepouillard@hotmail.fr>
2021-12-06 09:41:31 +0100

    add build

---
+++ build/precache-manifest.9871d1a8d8c481015b5ed8724c1c9888.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4bd713c9266e6e4c32fc5e56348717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac2f8a2a0e03b6eb124",
+    "url": "/static/css/main.a86086d9.chunk.css"
+  },
+  {
+    "revision": "6bfd322a36054c50e0fa",
+    "url": "/static/js/2.0b0ff490.chunk.js"
+  },
+  {
+    "revision": "9ac2f8a2a0e03b6eb124",
+    "url": "/static/js/main.ee065399.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "48066625e3365146aff1463adad51418",
+    "url": "/static/media/AS_Saint-Etienne.48066625.png"
+  },
+  {
+    "revision": "97c036f12d8fde9edc0fd2d1094d80d1",
+    "url": "/static/media/FC-Metz.97c036f1.png"
+  },
+  {
+    "revision": "b3331cce7f0067859ebafef17b0ad49f",
+    "url": "/static/media/FC_Nantes_logo.b3331cce.png"
+  },
+  {
+    "revision": "3fa93ca3e7c6a93025e5d6fefc9c93a4",
+    "url": "/static/media/Girondins_de_Bordeaux.3fa93ca3.png"
+  },
+  {
+    "revision": "dca2bc3b07879fceda86d7569e0cc529",
+    "url": "/static/media/Logo_AS_Monaco_FC.dca2bc3b.png"
+  },
+  {
+    "revision": "9324724c36002e7fe59091527f0d6952",
+    "url": "/static/media/Logo_Clermont_Foot.9324724c.png"
+  },
+  {
+    "revision": "2dcdf92c1160d5db6fa29d83b1f4ce8d",
+    "url": "/static/media/Logo_FC_Lorient.2dcdf92c.png"
+  },
+  {
+    "revision": "c7b2dee0b2bb3c44641cbaa5d493806f",
+    "url": "/static/media/Logo_LIGUE1.c7b2dee0.png"
+  },
+  {
+    "revision": "ec02c088be43bc95402b6089cb291214",
+    "url": "/static/media/Logo_LOSC_Lille.ec02c088.png"
+  },
+  {
+    "revision": "443276a90db703740330ce251d3d1f3c",
+    "url": "/static/media/Logo_OGC_Nice.svg.443276a9.png"
+  },
+  {
+    "revision": "ae9304b01be04423b33b5b3464f2b990",
+    "url": "/static/media/Logo_Olympique_de_Marseille.svg.ae9304b0.png"
+  },
+  {
+    "revision": "42e8cf83535e3f77a75081ef68436fdc",
+    "url": "/static/media/Logo_RC_Lens.42e8cf83.png"
+  },
+  {
+    "revision": "c4b0782875e32cbc4aa4a2f6e30181cd",
+    "url": "/static/media/Logo_SCO_Angers.c4b07828.png"
+  },
+  {
+    "revision": "2e109ec2f9394548d6f2300c21ede42f",
+    "url": "/static/media/Logo_Stade_Brestois.2e109ec2.png"
+  },
+  {
+    "revision": "91b08cff4d0ff0a9486f1dd178fea8d0",
+    "url": "/static/media/Logo_Stade_Rennais.91b08cff.png"
+  },
+  {
+    "revision": "525490ca1f6b79f6691f31d498e63ada",
+    "url": "/static/media/Logo_estac_troyes.525490ca.png"
+  },
+  {
+    "revision": "fbc24e15a61fafdbc4e6ac6422d086d8",
+    "url": "/static/media/Montpellier_Herault.fbc24e15.png"
+  },
+  {
+    "revision": "16ed875b7703dca925af2248d1c8ab2b",
+    "url": "/static/media/Olympique_lyonnais_logo.16ed875b.png"
+  },
+  {
+    "revision": "01019f3b27c5ae4ce9e308209d543194",
+    "url": "/static/media/Paris_Saint-Germain_Logo.svg.01019f3b.png"
+  },
+  {
+    "revision": "01710b1fc740dcd88280e22673a4f1bb",
+    "url": "/static/media/Racing_Club_de_Strasbourg.01710b1f.png"
+  },
+  {
+    "revision": "8b614c8ed96b138bf0aa9504ebfe5a53",
+    "url": "/static/media/ballon_foot.8b614c8e.svg"
+  },
+  {
+    "revision": "24fdc9b65d2f4a014549b3b2e204ea3b",
+    "url": "/static/media/carton_jaune.24fdc9b6.jpg"
+  },
+  {
+    "revision": "5904852cb050276875a5e76534c6e396",
+    "url": "/static/media/carton_rouge.5904852c.jpg"
+  },
+  {
+    "revision": "c3ecdda5e74f8386331829c21f4a780c",
+    "url": "/static/media/football_terrain.c3ecdda5.png"
+  },
+  {
+    "revision": "4cdcaae5345e279d2988d0253ad00b19",
+    "url": "/static/media/horloge.4cdcaae5.png"
+  },
+  {
+    "revision": "2d1d501b991e4af89e8231b753afbb1f",
+    "url": "/static/media/locali.2d1d501b.png"
+  },
+  {
+    "revision": "7976008b2c4bb93a03f8336d7a828f19",
+    "url": "/static/media/pin1.7976008b.png"
+  },
+  {
+    "revision": "d9ababdc5e2e49ef927bef00eca633f1",
+    "url": "/static/media/pin2.d9ababdc.png"
+  },
+  {
+    "revision": "f619a10868d5482b83c462e0e6642915",
+    "url": "/static/media/subs.f619a108.svg"
+  }
+]);
